7f1c6f8f007e32e905ecdb24f4c44e6d98c6edbf
fazlay <rfazlay21@gmail.com>
2024-10-25 23:13:03 +0600

    added table with tailwind css

--- public/assets/script.py
+++ public/assets/script.py
@@ -1,5 +1,6 @@
 import sys
 from io import StringIO
+import time
 
 # Redirect standard output to a string (to capture print statements)
 output = StringIO()
@@ -8,6 +9,13 @@ sys.stdout = output
 # Example Python code
 print("Hello from Python!")
 print("Running in the browser using Pyodide.")
+
+
+# Delay for 3 seconds
+time.sleep(3)
+
+# Print statement
+print("This message prints after a 3-second delay.")
 
 # Reset redirect.
 sys.stdout = sys.__stdout__
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
+import { columns } from "./payments/columns";
+import { ListComponent } from "./ListComponent";
 import PythonFileRunner from "./PythonFileRunner";
 
 import "./App.css";
 
 function App() {
+  const data = [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+  ];
   return (
     <>
       <PythonFileRunner />
+      <ListComponent columns={columns} data={data} />
     </>
   );
 }
